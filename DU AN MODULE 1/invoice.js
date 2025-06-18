aae54417939c4745f9867d8875b44ee9c3fbc688
dichthanhtuyen <dichthanhtuyen@gmail.com>
2025-06-18 18:19:33 +0700

    du an

---
+++ DU AN MODULE 1/invoice.js
@@ -0,0 +1,11 @@
+class Invoice{
+    id;
+    customer;
+    listProduct;
+constructor(id,cutomer,listProduct){
+    this.id = id;
+    this.cutomer = cutomer;
+    this.listProduct = [];
+}
+}
+
---
+++ DU AN MODULE 1/product.js
@@ -0,0 +1,12 @@
+class Product {
+    id;
+    name;
+    quantity;
+    price;
+    constructor(id, name, quantity, price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price
+    }
+}
